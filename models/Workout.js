54f61a4a2772e578c89edd4f7f3f315968cf9213
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-22 02:54:31 -0500

    adding content to Workout.js model

---
+++ models/Workout.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+const workoutSchema = new Schema(
+  {
+    // date
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    // exercise
+    exercise: [{
+
+      name: {
+        type: String
+      },
+
+      type: {
+        type: String,
+      },
+
+      weight: {
+        type: Number,
+      },
+
+      sets: {
+        type: Number
+      },
+
+      reps: {
+        type: Number
+      },
+
+      duration: {
+        type: Number
+      },
+
+      distance: {
+        type: Number
+      }
+
+    }],
+  },
+  {
+    toJSON: {
+      // ** need for summing durations and distances **
+      virtuals: true,
+    }
+  }
+);
+
+const Workout = model('workout', workoutSchema);
+
+module.exports = Workout;
