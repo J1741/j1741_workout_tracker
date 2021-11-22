5e6301e29041c1d3ede86f3d086b85b99db065b6
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-22 05:44:50 -0500

    successfully tested api route to get all workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
+const Workout = require('../models/Workout');
 
 // add api routes - controllers separate?
 router.get('/test', (req, res) => {
   console.log("in test api get route!")
   res.send('hello there!');
+})
+
+router.get('/test-all', (req, res) => {  
+  Workout.find({})
+    .then(testAllWorkouts => {
+      res.json(testAllWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    })
 })
 
 module.exports = router;
