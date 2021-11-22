ef7f98e21dc9edde667ee92c1ba1b2300e71ea82
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-22 06:00:19 -0500

    removing test routes from apiRoutes, adding get route for /api/workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,16 @@
 const router = require('express').Router();
 const Workout = require('../models/Workout');
 
-// test route to make sure api routes are working
-router.get('/test', (req, res) => {
-  console.log("in test api get route!")
-  res.send('hello there!');
-})
-
-// test route to get all workouts 
-router.get('/test-all', (req, res) => {  
+// get all workouts
+router.get('/workouts', (req, res) => {
+  console.log("** /api/workouts hit **");
   Workout.find({})
-    .then(testAllWorkouts => {
-      res.json(testAllWorkouts);
+    .then(allWorkouts => {
+      res.json(allWorkouts);
     })
     .catch(err => {
       res.json(err);
-    })
+    });
 })
 
 module.exports = router;
